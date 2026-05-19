30f9974084f65c04dfe4caa37627e93b79634214
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2026-05-19 12:20:28 +0000

    Create __init__.py

---
+++ tools/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+-Intricate nodal playground - tools/__init__.py package initializer
+-Tools package initializer for enjoying
+-Built using a single shared braincell by Yours Truly and various Intelligences
+"""
