3bdca10fbed604e92275133cdb157ade5ee309d1
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2026-05-19 12:20:25 +0000

    Create __init__.py

---
+++ tools/icon_pipeline/scripts/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+-Intricate nodal playground - tools/icon_pipeline/scripts/__init__.py package initializer
+-Scripts package initializer for enjoying
+-Built using a single shared braincell by Yours Truly and various Intelligences
+"""
